8a5b6326a6bb6935b1f37cf17739043ddc070746
EloxFire <eloxfire2014.ea@gmail.com>
2019-11-15 21:50:46 +0100

    Working Messages

--- app.js
+++ app.js
@@ -10,28 +10,23 @@ let server = http.createServer(function(req, res) {
 });
 
 // Chargement de socket.io
-let io = require('socket.io').listen(server);
+let io = require("socket.io").listen(server);
+let users = [];
+let connections = [];
 
-io.sockets.on('connection', function (socket, pseudo) {
-  // Quand un client se connecte, on lui envoie un message
-  // socket.emit('message', 'Vous êtes bien connecté !');
-  // On signale aux autres clients qu'il y a un nouveau venu
-  // socket.broadcast.emit('message', 'Un autre client vient de se connecter ! ');
+io.sockets.on("connection", function (socket) {
+  connections.push(socket);
+  console.log("Nouvelle connexion : %s sockets connectés", connections.length);
 
-  // Dès qu'on nous donne un pseudo, on le stocke en variable de session
-  socket.on('petit_nouveau', function(pseudo) {
-    socket.pseudo = pseudo;
+  socket.on("deconnexion", function(data){
+    connections.pop(connections.indexOf(socket), 1);
+    console.log("Nouvelle déconnexion : %s socket conectés", connections.length);
   });
 
-  // Dès qu'on reçoit un "message" (clic sur le bouton), on le note dans la console
-  socket.on('message', function (message) {
-    // On récupère le pseudo de celui qui a cliqué dans les variables de session
-    console.log(message);
-  });
-
-  socket.on('sendMessage', function (message) {
-    // On récupère le pseudo de celui qui a cliqué dans les variables de session
-    console.log(socket.pseudo + ' a envoyé : ' + message);
+  //Send Message
+  socket.on("send", function(data){
+    console.log(data);
+    io.sockets.emit('new message', {msg: data});
   });
 });
 
